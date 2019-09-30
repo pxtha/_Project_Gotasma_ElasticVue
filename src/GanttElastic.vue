e060dc2008a4ff014c7d527e5edf3371aed4c836
Thang Pham <pxthang97@gmail.com>
2019-09-30 09:57:22 +0700

    DefaultStyle

--- src/GanttElastic.vue
+++ src/GanttElastic.vue
@@ -57,7 +57,7 @@ function getOptions(userOptions) {
       type: 'type',
       style: 'style',
       collapsed: 'collapsed',
-      myAttribute: 'myAttribute',
+      estimateDuration: 'estimateDuration',
       effort: 'effort'
     },
     width: 0,
@@ -526,6 +526,9 @@ const GanttElastic = {
 
     fillTasks(tasks) {
       for (let task of tasks) {
+        if (typeof task.progress === 'undefined') {
+          task.progress = 0
+        }
         if (typeof task.user === 'undefined') {
           task.user = [];
         }
@@ -571,8 +574,8 @@ const GanttElastic = {
         if (typeof task.parent === 'undefined') {
           task.parent = null;
         }
-        if (typeof task.myAttribute === 'undefined') {
-          task.myAttribute = null;
+        if (typeof task.estimateDuration === 'undefined') {
+          task.estimateDuration = null;
         }
         if (typeof task.effort === 'undefined') {
           task.effort = 100;
@@ -582,17 +585,13 @@ const GanttElastic = {
         }
         if (typeof task.endTime === 'undefined' && task.hasOwnProperty('end')) {
           task.endTime = dayjs(task.end).valueOf(); // frick1
-        } else if (typeof task.endTime === 'undefined' && task.hasOwnProperty('duration')) {
-          task.myAttribute = task.duration        // gan tam duration
+        } else if ( typeof task.endTime === 'undefined' && task.hasOwnProperty('duration') ) {
+          task.estimateDuration = task.duration        // gan tam duration
           let timeStart = new Date(task.startTime);
           let calculateTimeChart = task.startTime;
           let dayofWeek = (timeStart.getDay());
           let durationDays = task.duration / 86400000;
           this.actualDuration = task.duration
-          // console.log('startTime: ' + dayjs(task.startTime).format('DD-MM-YYYY'))
-          // console.log('duration: ' + durationDays)
-          // console.log('dateofweeks ' + dayofWeek)
-          // console.log('ngay nghi list', this.exceptionDays)
           let isHoliday = false
           for (let i = 0; i < durationDays; i++) {
             for (let j = 0; j < this.exceptionDays.length; j++) {
